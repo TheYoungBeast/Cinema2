74e6bcd674b52ea1ac6132a5cd751ef0f05419ce
theyoungbeast <theyoungbeast>
2022-01-15 17:16:05 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -78,6 +78,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes, { /*enableTracing: true*/ })
   ],
