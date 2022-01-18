a7e53b16479a1c2fbee3986a1d56dfffeb92dd3d
theyoungbeast <theyoungbeast>
2022-01-18 15:33:19 +0100

    .

--- src/app/services/DataService/data-service.service.ts
+++ src/app/services/DataService/data-service.service.ts
@@ -74,8 +74,11 @@ export class DataService {
   }
 
   deleteMovie(id: number) {
+    if(this.serviceData.getValue().screenings.filter( s => s.movieId == id ).length)
+      throw Error("Unable to delete the movie. The movie is in use");
+
     this.httpSerivce.deleteMovie(id).subscribe( resp => {
-      if(resp.status !== 204) throwError(resp.statusText);
+      if(resp.status !== 204) throwError(() => new Error(resp.statusText));
 
       this.serviceData.getValue().movies.splice(id, 1);
       this.serviceData.next(this.serviceData.getValue());
@@ -83,8 +86,11 @@ export class DataService {
   }
 
   deleteRoom(id: number) {
+    if(this.serviceData.getValue().screenings.filter( r => r.roomId == id ).length)
+      throw Error("Unable to delete the room. The room is in use");
+
     this.httpSerivce.deleteRoom(id).subscribe( resp => {
-      if(resp.status !== 204) throwError(resp.statusText);
+      if(resp.status !== 204) throwError(() => new Error(resp.statusText));
 
       this.serviceData.getValue().rooms.splice(id, 1);
       this.serviceData.next(this.serviceData.getValue());
