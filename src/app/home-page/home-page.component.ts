5ab13a708c52f9dfb12be15d38a3443e3db1e3fd
Łukasz <chojecki2000@gmail.com>
2022-01-19 02:17:11 +0100

    home-page today's screenings

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { CinemaData } from '../interface/cinema-data';
+import { Screening } from '../interface/screening';
+import { DataService } from '../services/DataService/data-service.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css', '../css/ScreeningDetails.css']
+  styleUrls: ['./home-page.component.css', 
+  '../css/ModernTable.css',
+  '../css/ScreeningDetails.css',
+  '../css/FancyButtons.css' ]
 })
-export class HomePageComponent implements OnInit {
 
-  constructor() { }
+
+export class HomePageComponent implements OnInit {
+  data: CinemaData = {} as CinemaData;
+  todaysScreenings: Screening[] = [] as Screening[];
+  today: Date = new Date();
+
+  constructor(private cinemaDataService: DataService) { }
 
   ngOnInit(): void {
+    this.cinemaDataService.getData().subscribe( data => {
+      this.data = data;
+    })
+
+    // checks todays screenings
+    this.todaysScreenings = this.data.screenings.filter(s => this.isToday(s.date))
+  }
+
+  isToday = (date: Date) => {
+    const today = new Date()
+    console.log(today)
+    return date.getDate() == today.getDate() &&
+    date.getMonth() == today.getMonth() &&
+    date.getFullYear() == today.getFullYear()
   }
 
 }
